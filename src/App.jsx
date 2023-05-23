dbe019c0a360fae679eddc9ec54955f04d1ecc3a
Sonia Flores Zúñiga <zunigafloressonia105@gmail.com>
2023-05-23 09:59:08 -0600

    Trabajando con la API

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import Categories from "./componets/Categories"
+import GifsExpo from "./GifsExpo"
+
+function App() {
+  const [categories, setCategories] = useState(["Robotech", "Naruto"])
+
+  return (
+    <div className="m-5">
+      <h3>Gif Demo App</h3>
+      <hr />
+      <Categories
+        categories={categories}
+        setCategories={setCategories}
+      />
+      <hr />
+      <GifsExpo
+        categories={categories}
+        setCategories={setCategories}
+      />
+    </div>
+  )
+}
+
+export default App
